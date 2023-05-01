326d0b34a187d3645d427361c613f2d907ac72d9
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-04-30 21:52:50 -0300

    Update basico.js

--- react JS/basico.js
+++ react JS/basico.js
@@ -1,10 +1,1 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function app() {
-    return (
-        <View style={StyleSheet.container}></View>
-        <text>open  </text>
-    )
-}
+import React
