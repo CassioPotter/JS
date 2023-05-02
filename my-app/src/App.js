1f4ce9635fc07fb8cf5264878798074efe825fc7
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-02 13:20:16 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function olaMundo() {
-  return <h1>Ola Mundo</h1>
-}
+  return (<h1>Ola Mundo</h1>
+)}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Te amo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Não clique
-        </a>
-      </header>
+    <div>
+      <olaMundo/>
     </div>
   );
 }
