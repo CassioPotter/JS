2b95dfaadc07c430b421436860c4458429787502
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-02 22:16:28 -0300

    Update TESTE.js

--- react JS/TESTE.js
+++ react JS/TESTE.js
@@ -10,3 +10,23 @@ function parimp(n) {
 let res = parimp(4)
 
 console.log(res)
+
+function soma(n1 = 0, n2 = 0) {
+    return n1+n2
+}
+
+let som = soma(2, 2)
+
+console.log(som)
+
+function fatorial(n) {
+    let fat = 1
+    for (let c = n; c > 1; c--){
+        fat = fat*c
+    }
+    return fat
+}
+
+fatorial(5)
+
+console.log(fat)
