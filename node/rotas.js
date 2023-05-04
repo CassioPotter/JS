2a3743e56b4ea5e34e9a32aa42dd224bd4631c4e
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-04 18:10:54 -0300

    Update rotas.js

--- node/rotas.js
+++ node/rotas.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const app = express()
 
+app.get("/", function (req, res) {
+    res.send("Ola mundo")
+})
 app.listen(8081, function () {
     console.log("rodando")
 })
