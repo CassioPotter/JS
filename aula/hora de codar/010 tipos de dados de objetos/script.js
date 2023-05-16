470339c1497e16f58ca2b967d8118e1f7ecd22e4
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-16 14:38:38 -0300

    Create script.js

---
+++ aula/hora de codar/010 tipos de dados de objetos/script.js
@@ -0,0 +1,14 @@
+document.write('Objetos<br>')
+document.write('Funciona como array <br>')
+document.write('Guarda valores em uma unica variavel<br>')
+document.write('Precisa usar o : para receber valores <br>')
+document.write('Se separa com virgula')
+
+var obj = {
+    nome: "cassio",
+    idade: 35, 
+    prof: eletrica
+}
+
+document.write(obj)
+document.write(obj.nome)
