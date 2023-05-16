10c765bbbd160a1e25824b270a87bae0343bf2fc
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-16 14:52:19 -0300

    Create script.js

---
+++ aula/hora de codar/011 arrays/script.js
@@ -0,0 +1,11 @@
+document.write("Arrys <br>")
+document.write('São considerados objetos <br>')
+document.write('Pode ter dados de qualquer tipo <br>')
+
+var arr = ['cassio', 35, true]
+document.write(arr + '<br>')
+
+arr[4] = 'potter'
+document.write(arr)
+document.write('<br>')
+document.write(arr[4])
