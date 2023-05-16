eb402bc5adc6b09241f01818b63e8bc408180a41
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-16 14:38:46 -0300

    Create script.js

---
+++ aula/hora de codar/009 null e undefined/script.js
@@ -0,0 +1,12 @@
+document.write('null e undefind são tipos de dados<br>')
+document.write('Null representa um valor que a gente espera, vazio<br>')
+document.write('undefinder representa uma variavel esta criada mas ela não tem valor <br>')
+
+var nome = null
+var sobre;
+
+document.write(nome + '<br>')
+document.write(typeof (nome))
+document.write('<br>')
+document.write(sobre + '<br>')
+
