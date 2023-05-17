473ccba13c53403f08ac137313f5a704d9caa076
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-17 09:23:32 -0300

    Update script.js

---
+++ aula/hora de codar/012 if/script.js
@@ -0,0 +1,8 @@
+document.write('Executa se for verdadeiro <br>')
+document.write('Se for falso segue normalmente <br>')
+document.write('Do tipo booleano')
+document.write('<br>')
+document.write('<br>')
+document.write('if (teste logico) {<br>comando}<br><br>')
+document.write('var idade = 16 <br> if (idade > 18){<br> console.log ("pode dirigir")')
+
