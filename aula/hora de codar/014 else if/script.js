165d4c8d73c84330fff48b5a26b3e7715334d0dd
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-17 14:01:24 -0300

    Create script.js

---
+++ aula/hora de codar/014 else if/script.js
@@ -0,0 +1,5 @@
+document.write('if (teste lógico){<br>')
+document.write('comando}<br>')
+document.write('else if (teste logico caso o 1 seja negativo){<br>')
+document.write('comando}')
+document.write('else{comando caso as duas primeiras tenha dado negativo}')
