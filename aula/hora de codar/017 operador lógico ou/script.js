c8005e2814c06154cbbae8a01c7db4782d22b94b
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-05-17 18:42:12 -0300

    Create script.js

---
+++ aula/hora de codar/017 operador lógico ou/script.js
@@ -0,0 +1,1 @@
+document.write('<h1>>ou or ||</h1><br>Ele se torna verdadeiro se alguma opção for verdadeira e se torna falso quando as duas forem falsa<br><br>if (idade < 16 || idade > 65 ){<br> Nao precisa votar}')
